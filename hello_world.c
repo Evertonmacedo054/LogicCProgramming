64dba861d69c02a7b9d848727755921664b8b593
Evertonmacedo054 <evertonmacedo541@gmail.com>
2024-10-27 21:10:22 -0300

    correção das palavras com acento

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,14 @@
-#include <stdio.h>   // Biblioteca padr√£o de entrada e sa√≠da
-#include <locale.h>  // Biblioteca para configura√ß√£o de localidade
 
-int main() {
-    setlocale(LC_ALL, "Portuguese_Brazil");  // Configura a localidade para Portugu√™s do Brasil
-    printf("Ol√°, mundo!\n");  // Imprime "Ol√°, mundo!" na tela
+#include <stdio.h>   // Biblioteca padr„o de entrada e saÌda
+#include <locale.h>  // Biblioteca para configuraÁ„o de localidade
+
+int main(){
+    setlocale(LC_ALL, "Portuguese_Brazil");  // Configura a localidade para PortuguÍs do Brasil
+    printf("Ol·, Mundo!\n");  // Imprime "Ol·, Mundo!" na tela
+    printf("Ol·, Mundo 2!\n");  // Imprime "Ol·, Mundo 2!" na tela
     return 0;  // Retorna 0 para indicar que o programa terminou com sucesso
 }
+
+
+
+ 
