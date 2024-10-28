5ed82247c8d5e8157351ad921bab47db6a88bbf0
Evertonmacedo054 <evertonmacedo541@gmail.com>
2024-10-28 10:21:02 -0300

    removendo olá mundo 2!

--- C_Programs/hello_world.c
+++ C_Programs/hello_world.c
@@ -5,7 +5,6 @@
 int main(){
     setlocale(LC_ALL, "Portuguese_Brazil");  // Configura a localidade para Português do Brasil
     printf("Olá, Mundo!\n");  // Imprime "Olá, Mundo!" na tela
-    printf("Olá, Mundo 2!\n");  // Imprime "Olá, Mundo 2!" na tela
     return 0;  // Retorna 0 para indicar que o programa terminou com sucesso
 }
 
